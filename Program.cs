20e250c5ce27708f88872402b524810fb70c99a1
TomasDLV <tomas01dlv@gmail.com>
2023-07-05 19:14:16 -0300

    fin

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using EspacioCalculadora;
+using empresa;
 
 namespace MiAplicacion
 {
@@ -7,56 +7,68 @@ namespace MiAplicacion
     {
         static void Main(string[] args)
         {
-            Calculadora calculadora = new Calculadora();
+            Empleado empleado1 = new Empleado();
+            empleado1.Nombre = "Juan";
+            empleado1.Apellido = "Pérez";
+            empleado1.FechaNac = new DateTime(1980, 5, 15);
+            empleado1.EstadoCivil = 'S';
+            empleado1.Genero = 'M';
+            empleado1.FechaIngreso = new DateTime(2005, 7, 10);
+            empleado1.SueldoBasico = 65000;
+            empleado1.Cargo = Cargos.Ingeniero;
 
-            Console.WriteLine("¡Bienvenido a la calculadora!");
+            Empleado empleado2 = new Empleado();
+            empleado2.Nombre = "María";
+            empleado2.Apellido = "Gómez";
+            empleado2.FechaNac = new DateTime(1985, 9, 20);
+            empleado2.EstadoCivil = 'C';
+            empleado2.Genero = 'F';
+            empleado2.FechaIngreso = new DateTime(2010, 3, 5);
+            empleado2.SueldoBasico = 55000;
+            empleado2.Cargo = Cargos.Administrativo;
 
-            bool salir = false;
-            while (!salir)
+            Empleado empleado3 = new Empleado();
+            empleado3.Nombre = "Carlos";
+            empleado3.Apellido = "López";
+            empleado3.FechaNac = new DateTime(1975, 12, 8);
+            empleado3.EstadoCivil = 'C';
+            empleado3.Genero = 'M';
+            empleado3.FechaIngreso = new DateTime(2000, 1, 15);
+            empleado3.SueldoBasico = 75000;
+            empleado3.Cargo = Cargos.Especialista;
+
+            double totalSalarios = empleado1.CalcularSalario() + empleado2.CalcularSalario() + empleado3.CalcularSalario();
+            Console.WriteLine("Monto total de salarios: " + totalSalarios);
+
+            Empleado[] empleados = { empleado1, empleado2, empleado3 };
+
+            Empleado empleadoProximoJubilarse = null;
+            int aniosProximoJubilarse = int.MaxValue;
+
+            foreach (var empleado in empleados)
             {
-                Console.WriteLine("Ingrese una operación (sumar, restar, multiplicar, dividir, limpiar) o 'salir' para salir:");
-                string operacion = Console.ReadLine();
+                int aniosParaJubilarse = empleado.AniosParaJubilarse();
 
-                switch (operacion)
+                if (aniosParaJubilarse < aniosProximoJubilarse)
                 {
-                    case "sumar":
-                        Console.WriteLine("Ingrese el número a sumar:");
-                        float numSumar = float.Parse(Console.ReadLine());
-                        calculadora.Sumar(numSumar);
-                        Console.WriteLine("El resultado es: " + calculadora.Resultado);
-                        break;
-                    case "restar":
-                        Console.WriteLine("Ingrese el número a restar:");
-                        float numRestar = float.Parse(Console.ReadLine());
-                        calculadora.Restar(numRestar);
-                        Console.WriteLine("El resultado es: " + calculadora.Resultado);
-                        break;
-                    case "multiplicar":
-                        Console.WriteLine("Ingrese el número a multiplicar:");
-                        float numMultiplicar = float.Parse(Console.ReadLine());
-                        calculadora.Multiplicar(numMultiplicar);
-                        Console.WriteLine("El resultado es: " + calculadora.Resultado);
-                        break;
-                    case "dividir":
-                        Console.WriteLine("Ingrese el número divisor:");
-                        float numDividir = float.Parse(Console.ReadLine());
-                        calculadora.Dividir(numDividir);
-                        Console.WriteLine("El resultado es: " + calculadora.Resultado);
-                        break;
-                    case "limpiar":
-                        calculadora.Limpiar();
-                        Console.WriteLine("El resultado ha sido limpiado.");
-                        break;
-                    case "salir":
-                        salir = true;
-                        break;
-                    default:
-                        Console.WriteLine("Operación inválida. Intente nuevamente.");
-                        break;
+                    empleadoProximoJubilarse = empleado;
+                    aniosProximoJubilarse = aniosParaJubilarse;
                 }
             }
 
-            Console.WriteLine("¡Hasta luego!");
+            Console.WriteLine("Empleado más próximo a jubilarse:");
+            Console.WriteLine("Nombre: " + empleadoProximoJubilarse.Nombre);
+            Console.WriteLine("Apellido: " + empleadoProximoJubilarse.Apellido);
+            Console.WriteLine("Fecha de Nacimiento: " + empleadoProximoJubilarse.FechaNac.ToShortDateString());
+            Console.WriteLine("Estado Civil: " + empleadoProximoJubilarse.EstadoCivil);
+            Console.WriteLine("Género: " + empleadoProximoJubilarse.Genero);
+            Console.WriteLine("Fecha de Ingreso: " + empleadoProximoJubilarse.FechaIngreso.ToShortDateString());
+            Console.WriteLine("Sueldo Básico: " + empleadoProximoJubilarse.SueldoBasico);
+            Console.WriteLine("Cargo: " + empleadoProximoJubilarse.Cargo);
+            Console.WriteLine("Años para jubilarse: " + aniosProximoJubilarse);
+            Console.WriteLine("Salario correspondiente: " + empleadoProximoJubilarse.CalcularSalario());
+
+            
         }
     }
 }
